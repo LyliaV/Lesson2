e2ef0028ed7186ea9d72cde7f6db1012ba0894ea
LyliaV <edav17@bk.ru>
2025-03-21 22:08:29 +0300

    Задача 2

---
+++ src/task1/ex_01/Ex_01.java
@@ -0,0 +1,20 @@
+package task1.ex_01;
+
+import java.util.Scanner;
+
+public class Ex_01 {
+    public static void main(String[] args) {
+        new Ex_01().run();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите температуру в градусах Цельсия: ");
+
+        double celsius = scanner.nextDouble();
+        double fahrenheit = celsius * 9 / 5 + 32;
+
+        System.out.println("Температура в градусах Фаренгейта: " + fahrenheit);
+    }
+}
+
---
+++ src/task1/ex_02/Ex_02.java
@@ -0,0 +1,43 @@
+package task1.ex_02;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Ex_02 {
+    /**
+     * вычислять площадь круга по заданному радиусу. Формула для вычисления:
+     * S=π×r^2
+     * Где: r — радиус круга (тип double),
+     * π — константа (используйте Math.PI).
+     * Пример ввода:
+     * Введите радиус круга: 5
+     * Пример вывода:
+     * Площадь круга: 78.53981633974483
+     */
+
+    public static void main(String[] args) {
+        new Ex_02().run();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите радиус круга: ");
+
+        double radius = 0.0;
+
+        try {
+            radius = scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.print("Введите число!");
+            return;
+        };
+
+        double squareСircle = Math.PI * Math.pow( radius, 2 );
+
+        System.out.print("Площадь круга: " + squareСircle);
+
+    }
+
+
+}
